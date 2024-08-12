101ca20644b389de519c7fcadfbdcf5e9aa24e50
Traver Hart <traver@hart-lab.org>
2024-08-12 17:25:09 -0500

    add linear regression functions

--- Grape/grape.py
+++ Grape/grape.py
@@ -114,8 +114,11 @@ def get_mean_foldchange(fc_df, target_columns=0, mean_replicates=True, groupby_t
 
 	return mean_fc_df
 
-def load_genelist(filepath, sep='\t', index_col=0):
-	gene_df = pd.read_table(filepath, sep=sep, index_col=index_col)
+def load_genelist(filepath, sep='\t', index_col=0, names=['Genes']):
+	gene_df = pd.read_table(filepath, sep=sep, index_col=index_col, names=names)
+	#
+	# can be used for reference genes or regression gene pairs
+	#
 	return gene_df
 
 def mode_center(mean_fc_df):
@@ -129,9 +132,81 @@ def mode_center(mean_fc_df):
 def mode_center_vs_reference_genes(mean_fc_df, noness_genes):
 	# assumes a polished fold change df where the index is the target gene(s),
 	# NOT the unique guide ID
+	# (not currently implemented)
 	nonidx = [x for x in mean_fc_df.index.values if x in noness_genes]
 	modecenter_fc_df = mean_fc_df - mean_fc_df.loc[nonidx].median()
 	return modecenter_fc_df
+
+def make_predictor_matrix( fc_df, target_gene_list, gene_delim='_'):
+	predictor_matrix = pd.DataFrame( index=fc_df.index.values, columns=target_gene_list, data=0.)
+	for target_array in fc_df.index.values:
+		if gene_delim in target_array:
+			#
+			# this target is a gene pair
+			#
+			target_genes_in_array = target_array.split(gene_delim)
+			if ( len( np.intersect1d(target_genes_in_array, target_gene_list) ) == len(target_genes_in_array) ):
+				# both/all genes in the target array are in the genetic interaction gene list. include this genepair.
+				predictor_matrix.loc[target_array, target_genes_in_array] = 1
+		else:
+			# absence of delimiter implies single gene knockout. is the single gene in our target_gene_list?
+			if (target_array in target_gene_list):
+				predictor_matrix.loc[target_array, target_array] = 1
+	
+	# Drop rows not targeting a gene in the target list
+	dropme = np.where( predictor_matrix.sum(1)==0 )[0]  
+	predictor_matrix.drop( predictor_matrix.index.values[dropme], axis=0, inplace=True )
+
+	# Drop columns (genes) with no arrays targeting that gene
+	dropme = np.where( predictor_matrix.sum(0)==0 )[0]  
+	predictor_matrix.drop( predictor_matrix.columns.values[dropme], axis=1, inplace=True )
+
+	obs_vector = fc_df.loc[ predictor_matrix.index.values ]
+	print( 'regression matrix rows: {:5d}, cols: {:3d}'.format(predictor_matrix.shape[0], predictor_matrix.shape[1]) )
+	return predictor_matrix, obs_vector
+
+def filter_predictor():
+	# dummy function. should this be a function?
+	return
+
+def do_regression( predictor_matrix, obs_vector, fit_intercept=False, delimiter='_' ):
+	"""
+    Calculate the regression and provide the initial GI score.
+    Returns prediction (df), model (Logistic Regression object)
+
+    Parameters:
+    - predictor_matrix: dataframe of predictors. Usually binary. Columns = single genes, rows=single and double genes.
+    - obs_vector: dataframe of observed fold change, index is the same as predictor_matrix
+    - fit_intercept: whether to fit the intercept. Default false.
+    """
+	model = LinearRegression( fit_intercept=fit_intercept ).fit( predictor_matrix.values, obs_vector.values )
+	pred_fc = model.predict( predictor_matrix.values )
+	pairs = pd.DataFrame( index=predictor_matrix.index.values, columns=['fc_obs','fc_exp','GI_raw','g1_fc','g2_fc','dLFC'], data=0.)
+	pairs['fc_obs']  = obs_vector.values
+	pairs['fc_exp'] = pred_fc.flatten()  # already an array
+	# remove singles: columns of predictor matrix
+	single_genes = predictor_matrix.columns.values
+
+	singles = pairs.loc[single_genes,['fc_obs','fc_exp']]
+	pairs.drop( single_genes, axis=0, inplace=True )
+
+	pairs['GI_raw'] = pairs.fc_obs - pairs.fc_exp
+	#
+	# now for each pair, get g1, g2, dLFC based on observed FC in singles
+	for genepair in pairs.index.values:
+		g1, g2 = genepair.split(delimiter)
+		g1_fc = singles.loc[g1,'fc_obs']
+		g2_fc = singles.loc[g2,'fc_obs']
+		dLFC = pairs.loc[genepair, 'fc_obs'] - (g1_fc + g2_fc)
+		pairs.loc[genepair, ['g1_fc','g2_fc','dLFC']] = g1_fc, g2_fc, dLFC
+	#
+	# other data:
+	#
+	metadata = {}
+	metadata['Rsq'] = model.score( predictor_matrix.values, obs_vector.values)
+	metadata['Intercept'] = model.intercept_
+	metadata['Params']  = model.get_params()
+	return pairs, singles, metadata
 
 
 def get_args():
